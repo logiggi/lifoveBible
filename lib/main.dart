8a0026487d463b1160df53f0a5a50c0a983a0978
hankil241 <127019907+hankil241@users.noreply.github.com>
2024-05-30 14:07:14 +0900

    Add selecting version before multi view

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,26 @@
 import 'package:flutter/material.dart';
-import 'multi.dart'; // Import the MultiVersionPage widget
-import 'single.dart'; // Assuming your original MyHomePage is renamed to SingleVersionPage
+import 'multi.dart';
+import 'single.dart';
+import 'select.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  void updateSelectedBookAndChapter() {
-    // Iterate through bookChapters to find the selected book and chapter
-    int currentFileIndex = 0;
-    String? selectedBook;
-    int? selectedChapter;
-
-    for (var entry in bookChapters.entries) {
-      int chapterCount = entry.value;
-      if (currentIndex >= currentFileIndex &&
-          currentIndex < currentFileIndex + chapterCount) {
-        selectedBook = entry.key;
-        selectedChapter = currentIndex - currentFileIndex + 1;
-        break;
-      }
-      currentFileIndex += chapterCount;
-    }
-
-    setState(() {
-      this.selectedBook = selectedBook;
-      this.selectedChapter = selectedChapter;
-    });
-  }
-
-  List<int> getChapters(String book) {
-    int chapters = bookChapters[book]!;
-    return List<int>.generate(chapters, (index) => index + 1);
-  }
-
-  void loadSelectedChapterContent() {
-    if (selectedBook != null && selectedChapter != null) {
-      // Calculate the index of the selected chapter within the files array
-      int bookIndex = bookChapters.keys.toList().indexOf(selectedBook!);
-      int chapterIndex = selectedChapter! - 1;
-      currentIndex = 0;
-      for (int i = 0; i < bookIndex; i++) {
-        currentIndex += bookChapters.values.toList()[i];
-      }
-      currentIndex += chapterIndex;
-
-      if (currentIndex >= 0 && currentIndex < files.length) {
-        setState(() {
-          fileContent = File(files[currentIndex]).readAsStringSync();
-        });
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Selected chapter not found')),
-        );
-      }
-    }
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    return MaterialApp(
+      title: 'Bible App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => const SingleVersionPage(),
+        '/select': (context) => const VersionSelectionPage(),
         '/multi_version': (context) => const MultiVersionPage(),
       },
     );
