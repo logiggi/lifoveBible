4f23931132da2a1d14505eefada313443c348c5f
GwangjinJeong1 <dbsusqn12@gmail.com>
2024-06-03 01:19:44 +0900

    폰트사이즈 수정 Alert 수정

--- lib/setting.dart
+++ lib/setting.dart
@@ -43,7 +43,7 @@ class SettingPage extends StatelessWidget {
                   showDialog(
                       context: context,
                       builder: (context) {
-                        return const FontSettingDialog();
+                        return const FontSetting();
                       });
                 },
                 child: const ListTile(
@@ -71,89 +71,82 @@ class FontSetting extends StatelessWidget {
   Widget build(BuildContext context) {
     final settings = Provider.of<SettingProvider>(context);
     double tempSize = settings.fontSize;
-    return Container(
-        height: 354,
-        // color: AppColor.secondary,
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Text("FontSize",
-                style: defaultText.copyWith(
-                    fontSize: 16, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 22),
-            Container(
-                padding: const EdgeInsets.only(top: 16, bottom: 16),
-                // color: AppColor.primary,
-                child: const Row(
-                  children: [
-                    SizedBox(width: 32),
-                    Text("폰트 사이즈 조절", style: defaultText),
-                  ],
-                )),
-            Container(
-                // color: AppColor.primary,
-                child: Row(children: [
-              const SizedBox(width: 32),
-              Column(children: [
-                Slider(
-                    value: settings.fontSize,
-                    onChanged: (value) => settings.setFontSize(value),
-                    min: 10,
-                    max: 30),
-                const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text('작게'), Text('보통'), Text('크게')])
-              ])
-            ]))
-          ],
-        ));
-  }
-}
-
-class FontSettingDialog extends StatelessWidget {
-  const FontSettingDialog({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final settings = Provider.of<SettingProvider>(context);
-    double tempSize = settings.fontSize;
 
     return AlertDialog(
         title: const Text("Setting Font Size",
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        content: Column(
-          children: [
-            const Text("폰트 사이즈 조절", style: TextStyle(fontSize: 16)),
-            Slider(
-                value: tempSize,
-                onChanged: (value) {
-                  tempSize = value;
-                  settings.setFontSize(tempSize);
-                },
-                min: 10,
-                max: 30),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('작게', style: TextStyle(fontSize: 14)),
-                Text('보통', style: TextStyle(fontSize: 14)),
-                Text('크게', style: TextStyle(fontSize: 14))
-              ],
-            )
-          ],
+        content: SingleChildScrollView(
+          child: IntrinsicHeight(
+            child: Container(
+              width: 300,
+              padding: const EdgeInsets.only(top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("폰트 사이즈 조절", style: TextStyle(fontSize: 16)),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                              overlayShape: SliderComponentShape.noOverlay),
+                          child: Slider(
+                              value: tempSize,
+                              onChanged: (value) {
+                                tempSize = value;
+                                settings.setFontSize(tempSize);
+                              },
+                              min: 10,
+                              max: 30),
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('작게', style: TextStyle(fontSize: 14)),
+                            Text('보통', style: TextStyle(fontSize: 14)),
+                            Text('크게', style: TextStyle(fontSize: 14))
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Text(
+                      "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.(John 3:16)",
+                      style: defaultText.copyWith(fontSize: tempSize))
+                ],
+              ),
+            ),
+          ),
         ),
         actions: [
-          ElevatedButton(
-              onPressed: () {
-                settings.setFontSize(tempSize);
-                Navigator.of(context).pop();
-              },
-              child: const Text("Save", style: TextStyle(fontSize: 16))),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("Cancel", style: TextStyle(fontSize: 16)))
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 140,
+                height: 40,
+                child: ElevatedButton(
+                    onPressed: () {
+                      settings.setFontSize(tempSize);
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Save", style: TextStyle(fontSize: 16))),
+              ),
+              SizedBox(
+                width: 140,
+                height: 40,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child:
+                        const Text("Cancel", style: TextStyle(fontSize: 16))),
+              )
+            ],
+          )
         ]);
   }
 }
